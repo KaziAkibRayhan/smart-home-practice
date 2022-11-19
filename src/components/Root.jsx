572a1eabae9c5222880d2a87bd316fb6052aaf82
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-19 18:43:34 +0600

    add data in locale storage

--- src/components/Root.jsx
+++ src/components/Root.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
-import { Outlet } from "react-router-dom";
+import { Outlet, useLoaderData } from "react-router-dom";
+import { createContext } from "react";
+
+export const ProductContext = createContext([]);
+export const CartContext = createContext([]);
 
 const Root = () => {
+  const [cart, setCart] = useState([]);
+  const products = useLoaderData();
   return (
-    <div>
-      <Header />
-      <Outlet />
-      <Footer />
-    </div>
+    <ProductContext.Provider value={products}>
+      <CartContext.Provider value={[cart, setCart]}>
+        <Header />
+        <Outlet />
+        <Footer />
+      </CartContext.Provider>
+    </ProductContext.Provider>
   );
 };
 
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,11 +1,22 @@
 import { createBrowserRouter } from "react-router-dom";
-import Header from "../components/Header";
 import Root from "../components/Root";
+import Home from "../components/Home";
+import About from "../components/About";
+import ErrorPage from "../components/ErrorPage";
+import Shop from "../components/Shop";
 
 const router = createBrowserRouter([
   {
     path: "/",
+    loader: () => fetch("products.json"),
     element: <Root />,
+    errorElement: <ErrorPage />,
+    children: [
+      { path: "/", element: <Home /> },
+      { path: "/home", element: <Home /> },
+      { path: "/shop", element: <Shop /> },
+      { path: "/about", element: <About /> },
+    ],
   },
 ]);
 
