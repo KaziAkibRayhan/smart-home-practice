58205882b27e785a299c8c9864e574ffe082bf17
Akib Rayhan <kaziakibrayhan2018@gmail.com>
2022-11-19 22:34:19 +0600

    completed site

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import { CartContext } from "./Root";
 
 const Header = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [cart, setCart] = useContext(CartContext);
+  const [cart] = useContext(CartContext);
   return (
     <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
       <div className="relative flex items-center justify-between">
